de769428830718e43709e5d8f74a8690f33c170d
teamalmhia4 <74507447+teamalmhia4@users.noreply.github.com>
2020-12-11 14:57:13 +0300

    Update d1.js

--- d1.js
+++ d1.js
@@ -19,7 +19,7 @@ $(' div#l1 button.btn.btn-primary ,div#l2 button.btn.btn-primary , div#l3 button
 catch(e){console.log(e)}
 
 $(`<!--<a class="fl fa fa-image btn ui-corner-all ui-shadow ui-btn ui-btn-inline borderg" style="color:purple;margin:2px;width: 106px;text-align: center;font-size:15px!important;"><span dir="rtl"> (0) </span><span style="font-size: 15px!important;>يزن </span></a>-->
-<a href="https://api.whatsapp.com/send?phone=905349315009&amp;text=&amp;source=&amp;data=/" target="_blank" style="width:107px; margin-right: 5px;
+<a href="https://api.whatsapp.com/send?phone=&amp;text=&amp;source=&amp;data=/" target="_blank" style="width:107px; margin-right: 5px;
 margin-left: 2px;text-align: center;float: right;border-radius: 5px;
 padding: 3px;font-family: 'Cairo', sans-serif, Fontawesome;font-size: 13px!important;" class="SLPBeats fab fa-angellist" >تصميم :  يزن المالكي ⌁ للتواصل اضغط هنا/a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
 setTimeout (function (){  $("<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;'>تعديل و تصميم: <a href='https://api.whatsapp.com/send?phone=&amp;text=&amp;source=&amp;data=/'> يزن المالكي © 2021 0</a></div>").insertAfter($(".loginstat"));  },1000);
