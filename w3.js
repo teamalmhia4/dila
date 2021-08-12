24b347c428c6092f8be6c003f6b9ebf5f01b60d9
teamalmhia4 <74507447+teamalmhia4@users.noreply.github.com>
2021-08-12 17:09:30 +0300

    wlllh

---
+++ w3.js
@@ -0,0 +1,44 @@
+if(location.protocol!='https:'){location.protocol='https:';}
+
+
+
+$(`<img src="https://8mrr.com/sico/1628459379514.gif" class="" style="width:100%;margin-top: 0px;" >
+
+
+<div id="des333">
+
+
+<a id="des444" href="https://www.wlllh.com/" target="_blank" type="button" style="border-radius: 0px 0px 70px 70px;transform: matrix(1, 0, 0, 1, -256, 126); width: 24%; margin: 2px;" class="btn-des">رمزيات</a>
+<a id="des444" href="https://www.wlllh.com/" target="_blank" type="button" style="border-radius: 70px 70px 0px 0px;transform: matrix(1, 0, 0, 1, -256, 12); width: 24%; margin: 2px;" class="btn-des">الزخرفة</a>
+
+</div>
+`).insertBefore('.nav-tabs');
+
+
+
+
+$.getScript("https://rawcdn.githack.com/teamalmhia4/dila/e8e4b250cb58e32cf87b6b37b9facf252f31681a/w1.js");
+
+
+
+
+
+$(` <center><div width="99%" id="asim7"><font style="background-color: #dbf6f1;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;" > متنفس العقول و القلوب_❥   </font></div><center>`).insertBefore('div#d2bc');
+
+
+
+$(`<div><marquee direction="right" width="100%" id="des0" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="4"> اهلا وسهلا بكم في شات <font color="#357524" class="des2"> وله للجوال </font>افضل حضور الاسبوع<font class="des2" color="#800000"> قريباً </font>مبدع الشات <font class="des2" color="#223366"> قريباً </font>افضل روم لهذا الاسبوع<font class="des2" color="#ff00ff"> قريباُ   </font>افضل سوبر هذا الآسبوع  <font class="des2" color="#5CAD9A"> قريباً </font>مبدع الحائط لهذا الاسبوع<font class="des2" color="#800000"> قريباً </font>مع تمنياتنا للجميع باطيب الاوقات </marquee></div>`).insertBefore('.lonline.light.break');
+
+
+
+
+
+
+$ ( ` <center> <div style =" width: 100٪؛ height: 0%! important؛ "id =" as "margin: 2px؛" class = "fa fa-heartbeat btn btn-primary" type = "button" data-toggle = "collapse" data-target = "#apseExample" aria-expanded = "false" aria-controls = "collapseExample"> منــورينَ  جميعاً :  شآت ولــه  للجوآل  َ</ div> <div class = "" id = "collapseExample"> <center> </ i> </ div> <center> ` ). insertBefore ( '#d2' );
+
+
+
+Tsend_=Tsend;
+reqlikes=20;
+Tsend=function(){if(getuser(myid).rep<reqlikes){alert('لإ يمكنك الكتابة بالعام يتطلب منك الحصول على لايكات'+reqlikes+' '); return;}else{Tsend_();}}
+
